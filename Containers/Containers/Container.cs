c53f37fec9684540db29858d351a793860847406
3l3ctricPhilip <3l3ctricPhilip>
2024-03-18 18:27:27 +0100

    I started the project

---
+++ Containers/Containers/Container.cs
@@ -0,0 +1,11 @@
+namespace Containers;
+
+public class Container
+{
+    private int Weight { get; set; }
+    private int Height { get; set; }
+    private int NettoWeight { get; set; }
+    private int Depth { get; set; }
+    
+    
+}
